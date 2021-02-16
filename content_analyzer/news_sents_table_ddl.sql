df4f4a2d52ea20192b9281d7d80303b55ff1abb8
gary <sheng-fenghsu2019@u.northwestern.edu>
2021-02-16 21:22:32 +0800

    Update the sentence splitter

--- content_analyzer/news_sents_table_ddl.sql
+++ content_analyzer/news_sents_table_ddl.sql
@@ -5,8 +5,8 @@ CREATE TABLE news_db.`news_sents` (
   `news_id` int(11) NOT NULL,
   `sent_index` int(11) NOT NULL,
   `sent` varchar(10500) CHARACTER SET utf8mb4 NOT NULL,
-  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`news_sent_id`),
   UNIQUE KEY `news_sent_id_UNIQUE` (`news_sent_id`),
   FOREIGN KEY (news_id) REFERENCES news_db.news_contents(news_id)
--- content_analyzer/sent_splitter.py
+++ content_analyzer/sent_splitter.py
@@ -1,9 +1,11 @@
 import logging
-import os 
+import os, sys
 import time
 import re
 import mysql.connector
 import pickle
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
 
 from db_func import query_from_db
 
@@ -14,8 +16,13 @@ FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 logging.basicConfig(level=logging.INFO, filename=os.path.join(DIR_PATH, 'sent_splitter.log'), filemode='a', format=FORMAT)
 raw_df = query_from_db("SELECT news_id, news FROM news_contents WHERE processed_status = 0 LIMIT 100;")
 
-with open(os.path.join(DIR_PATH,'server2server.config'), 'rb') as f:
+
+
+with open(os.path.join(parent_dir, 'configs', 'loc2server.config'), 'rb') as f:
     configs = pickle.load(f)
+
+author_header = r'^((（.+?）)|(\(.+?\))|(【.+?】)|(〔.+?〕)|(\[.+?\])|(［.+?］))\s*'
+content_footer = r'^更多.*?報導：?$|^更多新聞推薦|^【更多新聞】|^延伸閱讀：|^【延伸閱讀】|^超人氣$|^看更多.*?文章|^更多匯流新聞網報導：|^原始連結|^更多\w+內容：|^《TVBS》提醒您：|^※|^（延伸閱讀：|^相關影音：|^責任編輯：|^☆|^更多\w+相關新聞|►'
 
 
 mydb = mysql.connector.connect(
@@ -25,6 +32,13 @@ mydb = mysql.connector.connect(
     database = configs['database']
     )
 
+def remove_header(author_header, header, sent):
+    if not re.search(author_header, sent):
+        return header, sent
+    else:
+        header_end = re.search(author_header, sent).end()
+        return remove_header(author_header, header + sent[:header_end],sent[header_end:])
+    
 def insert_process(sql, val):
     process_cursor = mydb.cursor()
     try:
@@ -42,20 +56,27 @@ def insert_sents(raw_df):
         val_status = (news_id, )
         insert_process(sql_status, val_status)
 
+        author_list = []
         sent_list = []
-        for sent in re.split(r'。\s+|\n', content):
-            if '延伸閱讀' in sent:
-         	   break
-            if len(sent):
-                sent_list.append(sent + '。')
+        for index, sent in enumerate(re.split(r'\n', content)):
+            if re.search(content_footer, sent):
+                break
+            if index == 0 and len(sent) < 20 and not re.search('。', sent):
+                author_list.append(sent)
+                continue
+            if index <= 1 and re.search(author_header, sent):
+                ori_sent = sent
+                header, sent = remove_header(author_header, '',  ori_sent)
+                author_list.append(header)
+            if len(sent) > 1:
+                sent_list.append(sent)
 
         for sent_id, sent in enumerate(sent_list):
             sent_cursor = mydb.cursor()
             try: 
                 sent_cursor.execute("INSERT INTO news_sents (news_id, sent_index, sent) VALUES (%s, %s, %s)", (news_id, sent_id + 1, sent))
-                
             except Exception as e:
-                logging.error('Error: {}\nSentence ID: {}'.format(e, sent_id))
+                logging.error('Error: {}\nSentence ID: {}\nNews_id ID: {}'.format(e, sent_id, news_id))
                 mydb.rollback()
             else:
                 mydb.commit()
