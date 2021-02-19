f9568182f94afa6341632bcb5bfff258ca6e94e2
gary <sheng-fenghsu2019@u.northwestern.edu>
2021-02-19 22:43:51 +0800

    update the sent processor

--- content_analyzer/nlp_processes_table_ddl.sql
+++ content_analyzer/nlp_processes_table_ddl.sql
@@ -1,12 +1,9 @@
-DROP TABLE IF EXISTS news_db.`nlp_processes`;
+DROP TABLE IF EXISTS news_db.`sent_processes`;
 
-CREATE TABLE news_db.`nlp_processes` (
-  `nlp_processes_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE news_db.`sent_processes` (
   `news_sent_id` int(11)  NOT NULL,
   `process_name` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`nlp_processes_id`),
-  UNIQUE KEY `nlp_processes_id_UNIQUE` (`nlp_processes_id`),
   FOREIGN KEY (news_sent_id) REFERENCES news_db.news_sents(news_sent_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
--- content_analyzer/sent_processor.py
+++ content_analyzer/sent_processor.py
@@ -1,20 +1,34 @@
-from ckiptagger import WS, POS, NER
-from db_func import query_from_db
-import ast
+import os, sys
+from ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger, CkipNerChunker
 import pickle
 import logging
 import mysql.connector
 import time
+import spacy
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parent_dir)
+
+from db_func import query_from_db
+
+# Initialize drivers
+ws_driver  = CkipWordSegmenter(level=3)
+pos_driver = CkipPosTagger(level=3)
+ner_driver = CkipNerChunker(level=3)
+
+
+nlp = spacy.load("zh_core_web_trf")
+os.environ['KMP_DUPLICATE_LIB_OK']= 'True'
+
+
+FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+logging.basicConfig(level=logging.INFO, filename=os.path.join(DIR_PATH, 'logs', 'sent_splitter.log'), filemode='a', format=FORMAT)
+
+
 start = time.time()
-ws = WS("./data")
-pos = POS("./data")
-ner = NER("./data")
 
 
-logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
-logging.root.setLevel(level=logging.INFO)
-
-with open('server2server.config', 'rb') as f:
+with open(os.path.join(parent_dir, 'configs', 'server2server.config'), 'rb') as f:
     configs = pickle.load(f)
 
 mydb = mysql.connector.connect(
@@ -23,26 +37,51 @@ mydb = mysql.connector.connect(
     passwd = configs['passwd'],
     database = configs['database'])
 
-def insert_sentlevel_info(word_sentence, pos_sentence, news_sent_id):
+def insert_sentlevel_info(word_sentence: list, pos_sentence:list , news_sent_id, ws_approach, dep_sentence = ''):
     assert len(word_sentence) == len(pos_sentence)
-    for word_index, (word, word_pos) in enumerate(zip(word_sentence, pos_sentence)):
-        word_cursor = mydb.cursor()
-        try:
-            word_cursor.execute("INSERT INTO news_db.news_words (news_sent_id, word_index, word, word_pos) VALUES (%s, %s, %s, %s)", (news_sent_id, word_index, word, word_pos))
-        except Exception as e:
-            logging.error('Insert word pos error: {}\n News Sent ID: {}'.format(e, news_sent_id))
-            mydb.rollback()
-        else:
-            mydb.commit()
-        word_cursor.close()
-        
-def insert_ner_info(entity_sent_list, news_sent_id):
-    for ent_id, (start_index, end_index, ent_type, ent_text) in enumerate(sorted(entity_sent_list, key = lambda x:x[0])):
+    if ws_approach == 'ckip-transformer':
+        for word_index, (word, word_pos) in enumerate(zip(word_sentence, pos_sentence)):
+            word_cursor = mydb.cursor()
+            try:
+                word_cursor.execute("INSERT INTO news_db.news_words (news_sent_id, word_index, word, word_pos, ws_approach) VALUES (%s, %s, %s, %s, %s)", (news_sent_id, word_index, word, word_pos, ws_approach))
+                #print("INSERT INTO news_db.news_words (news_sent_id, word_index, word, word_pos, ws_approach) VALUES ({}, {}, {}, {}, {})".format(news_sent_id, word_index, word, word_pos, ws_approach))
+
+            except Exception as e:
+                logging.error('Insert word pos error: {}\n News Sent ID: {}'.format(e, news_sent_id))
+                print('Insert word pos error: {}\nNews Sent ID: {}\nWs_approach: {}'.format(e, news_sent_id, ws_approach))
+                mydb.rollback()
+            else:
+                #print("else")
+                mydb.commit()
+            word_cursor.close()
+    elif ws_approach == 'spacy-transformer':
+        for word_index, (word, word_pos, word_dep) in enumerate(zip(word_sentence, pos_sentence, dep_sentence)):
+            word_cursor = mydb.cursor()
+            try:
+                word_cursor.execute("INSERT INTO news_db.news_words (news_sent_id, word_index, word, word_pos, word_dep, ws_approach) VALUES (%s, %s, %s, %s, %s, %s)", (news_sent_id, word_index, word, word_pos, word_dep, ws_approach))
+                #print("INSERT INTO news_db.news_words (news_sent_id, word_index, word, word_pos, word_dep, ws_approach) VALUES ({}, {}, {}, {}, {}, {})".format(news_sent_id, word_index, word, word_pos, word_dep, ws_approach))
+
+            except Exception as e:
+                logging.error('Insert word pos error: {}\n News Sent ID: {}'.format(e, news_sent_id))
+                print('Insert word pos error: {}\nNews Sent ID: {}\nWs_approach: {}'.format(e, news_sent_id, ws_approach))
+                mydb.rollback()
+            else:
+                mydb.commit()
+            word_cursor.close()
+    else:
+        logging.error("Error ws_approach")
+        sys.exit(0)
+
+def insert_ner_info(entity_sent_list, news_sent_id, ner_approach):
+    for ent_id, (ent_text, ent_type, ent_index) in enumerate(sorted(entity_sent_list, key = lambda x:x[2][0])):
         ner_cursor = mydb.cursor()
         try:
-            ner_cursor.execute("INSERT INTO news_db.news_ners (news_sent_id, start_index, end_index, ent_type, ent_text) VALUES (%s, %s, %s, %s, %s)", (news_sent_id, start_index, end_index, ent_type, ent_text))
+            ner_cursor.execute("INSERT INTO news_db.news_ners (news_sent_id, start_index, end_index, ent_type, ent_text, ner_approach) VALUES (%s, %s, %s, %s, %s, %s)", (news_sent_id, ent_index[0], ent_index[1], ent_type, ent_text, ner_approach))
+            #print("INSERT INTO news_db.news_ners (news_sent_id, start_index, end_index, ent_type, ent_text, ner_approach) VALUES ({}, {}, {}, {}, {}, {})".format(news_sent_id, ent_index[0], ent_index[1], ent_type, ent_text, ner_approach))
+        
         except Exception as e:
             logging.error('Insert NER error: {}\n News Sent ID: {}'.format(e, news_sent_id))
+            print('Insert NER error: {}\n News Sent ID: {}'.format(e, news_sent_id))
             mydb.rollback()
         else:
             mydb.commit()
@@ -51,9 +90,11 @@ def insert_ner_info(entity_sent_list, news_sent_id):
 def insert_process_flag(news_sent_id, process_name):
     process_cursor = mydb.cursor()
     try:
-        process_cursor.execute("INSERT INTO news_db.nlp_processes (news_sent_id, process_name) VALUES (%s, %s)", (news_sent_id, process_name))
+        process_cursor.execute("INSERT INTO news_db.sent_processes (news_sent_id, process_name) VALUES (%s, %s)", (news_sent_id, process_name))
+        #print("INSERT INTO news_db.nlp_processes (news_sent_id, process_name) VALUES ({}, {})".format(news_sent_id, process_name))
     except Exception as e:
         logging.error('Process Insert Error: {}\nNews sent ID: {}'.format(e, news_sent_id))
+        #print('Process Insert Error: {}\nNews sent ID: {}'.format(e, news_sent_id))
         mydb.rollback()
     else:
         mydb.commit()
@@ -63,18 +104,25 @@ def sent_level_analysis(raw_df):
     for index, (content_sent_id, sent) in raw_df.iterrows():
         insert_process_flag(content_sent_id, 'sent-analysis')
         try:
-            word_sentence_list = ws([sent])
-            pos_sentence_list = pos(word_sentence_list)
-            entity_sentence_list = ner(word_sentence_list, pos_sentence_list)
+            word_sentence_list  = ws_driver(sent, use_delim=False)
+            entity_sentence_list = ner_driver(sent, use_delim=False)
+            pos_sentence_list = pos_driver(word_sentence_list, use_delim=False)
+            spacy_doc = nlp(sent)
+            word_sent_list_spacy, word_pos_list_spacy, word_dep_list_spacy = zip(*[(token.text, token.tag_, token.dep_) for token in spacy_doc])
+            entity_sent_list_spacy = [(ent.text, ent.label_, (ent.start_char, ent.end_char)) for ent in spacy_doc.ents]
         except Exception as e:
             logging.error('NLP process Error: {}\n Content ID: {}'.format(e, content_sent_id))
+            print('NLP process Error: {}\n Content ID: {}'.format(e, content_sent_id))
         
         
-        insert_sentlevel_info(word_sentence_list[0],  pos_sentence_list[0], content_sent_id)
-        insert_ner_info(entity_sentence_list[0], content_sent_id)
+        insert_sentlevel_info(word_sentence_list[0],  pos_sentence_list[0], content_sent_id, 'ckip-transformer')
+        insert_sentlevel_info(word_sent_list_spacy, word_pos_list_spacy, content_sent_id, 'spacy-transformer', word_dep_list_spacy)
+        insert_ner_info(entity_sentence_list[0], content_sent_id, 'ckip-transformer')
+        insert_ner_info(entity_sent_list_spacy, content_sent_id, 'spacy-transformer')
      
 
-raw_df = query_from_db("SELECT nns.news_sent_id, nns.sent FROM news_db.news_sents nns WHERE nns.news_sent_id not in (SELECT nlp.news_sent_id FROM news_db.nlp_processes nlp WHERE nlp.process_name = \'sent-analysis\') LIMIT 1000")
+raw_df = query_from_db("SELECT nns.news_sent_id, nns.sent FROM news_db.news_sents nns WHERE not exists (SELECT 1 FROM news_db.sent_processes sp WHERE sp.process_name = \'sent-analysis\' and sp.news_sent_id = nns.news_sent_id) LIMIT 100")
 sent_level_analysis(raw_df)
 mydb.close()
 logging.error('Finish process {} examples in {} seconds'.format(len(raw_df), time.time() - start))
+print('Finish process {} examples in {} seconds'.format(len(raw_df), time.time() - start))
