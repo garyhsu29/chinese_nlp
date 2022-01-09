c33e49d775b7b8da0d08002ed72ed0a7decf02d0
Sheng-Feng Hsu <36487313+garyhsu29@users.noreply.github.com>
2022-01-10 05:06:27 +0800

    Update ddl and title processor
    Update ddl

--- content_analyzer/nlp_processes_table_ddl.sql
+++ content_analyzer/nlp_processes_table_ddl.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS news_db.`sent_processes`;
+DROP TABLE IF EXISTS news_db.`news_processes`;
 
-CREATE TABLE news_db.`sent_processes` (
-  `news_sent_id` int(11)  NOT NULL,
+CREATE TABLE news_db.`news_processes` (
+  `news_id` int(11)  NOT NULL,
   `process_name` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (news_sent_id) REFERENCES news_db.news_sents(news_sent_id)
+  FOREIGN KEY (news_id) REFERENCES news_db.news_contents(news_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
