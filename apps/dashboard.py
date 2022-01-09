7ab83743485e214178b21eaef19ad31700154e94
Sheng-Feng Hsu <36487313+garyhsu29@users.noreply.github.com>
2022-01-10 06:12:07 +0800

    update the dashboard queries
    update the dashboard queries

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -21,55 +21,58 @@ font = os.path.join(cur_dir_name, 'SourceHanSansTW-Regular.otf')
 @st.cache()
 def get_rss_data():
 	count_df = query_from_db("""
-	SELECT DATE(nrf.created_at) AS DATE, nc.category_name, COUNT(*) AS COUNT
+	SELECT DATE(nrf.created_at) AS DATE, nrf.rss_source, COUNT(*) AS COUNT
 	FROM news_db.news_rss_feeds nrf
-	LEFT JOIN news_db.news_categories nc ON nc.rss_source = nrf.rss_source
-	GROUP BY DATE(nrf.created_at), nc.category_name ORDER BY DATE(nrf.created_at);""")
+	GROUP BY DATE(nrf.created_at), nrf.rss_source ORDER BY DATE(nrf.created_at);""")
 	return count_df
 
 @st.cache()
 def get_success_parse_data():
 	success_df = query_from_db("""
-		SELECT DATE(nrf.updated_at) AS DATE, nc.category_name, COUNT(*) AS COUNT
+		SELECT DATE(nrf.updated_at) AS DATE, nrf.rss_source, COUNT(*) AS COUNT
 		FROM news_db.news_rss_feeds nrf
-		LEFT JOIN news_db.news_categories nc ON nc.rss_source = nrf.rss_source
 		WHERE nrf.processed_status = 1 and nrf.processed_success = 1
-		GROUP BY DATE(nrf.updated_at), nc.category_name ORDER BY DATE(nrf.updated_at);""")
+		GROUP BY DATE(nrf.updated_at), nrf.rss_source ORDER BY DATE(nrf.updated_at);""")
 	return success_df
 @st.cache()
 def get_fail_parse_data():
 	fail_df = query_from_db("""
-		SELECT DATE(nrf.updated_at) AS DATE, nc.category_name, COUNT(*) AS COUNT
+		SELECT DATE(nrf.updated_at) AS DATE, nrf.rss_source, COUNT(*) AS COUNT
 		FROM news_db.news_rss_feeds nrf
-		LEFT JOIN news_db.news_categories nc ON nc.rss_source = nrf.rss_source
 		WHERE nrf.processed_status = 1 and nrf.processed_success = 0
-		GROUP BY DATE(nrf.updated_at), nc.category_name ORDER BY DATE(nrf.updated_at);""")
+		GROUP BY DATE(nrf.updated_at), nrf.rss_source ORDER BY DATE(nrf.updated_at);""")
 	return fail_df
 
 @st.cache(allow_output_mutation=True)
 def get_keywords_by_date():
-	kw_df = query_from_db("""SELECT ADDTIME(news_published_date, '8:00:0') published_date, news_keywords 
+	kw_df = query_from_db("""SELECT published_date, news_keywords 
 		FROM news_db.news_contents
-		WHERE news_keywords is not NULL and news_keywords != '' and ADDTIME(news_published_date, '8:00:0') > '2021-01-19';""")
-	kw_df['published_date'] = kw_df['published_date'].dt.date
+		WHERE news_keywords is not NULL and news_keywords != '' and published_date > '2021-01-19';""")
 	return kw_df
 
 @st.cache(allow_output_mutation = True)
-def get_company_overview():
-	company_df = query_from_db("""SELECT ent_text as company_name, cast(ADDTIME(news_published_date, '8:00:0') as DATE) as published_date, COUNT(*) as count
-		FROM news_db.news_kw_view 
-		WHERE ent_text in (SELECT company_name FROM news_db.yahoo_stock_companies) and ent_type = 'ORG' and ADDTIME(news_published_date, '8:00:0') > '2021-01-19'
-		GROUP BY ent_text, cast(ADDTIME(news_published_date, '8:00:0') as DATE);""")
+def get_company_overview(selected_date):
+	company_df = query_from_db("""SELECT nkv.ent_text as company_name, COUNT(*) as count
+		FROM news_db.news_kw_view  nkv
+		WHERE EXISTS (SELECT 1 FROM news_db.yahoo_stock_companies ysc WHERE nkv.ent_text = ysc.company_name) and ent_type = 'ORG' and published_date = '{}'
+		GROUP BY ent_text""".format(selected_date))
 	return company_df
 
 def get_ner_by_date(selected_date):
-	ner_df = query_from_db("""SELECT CAST(nkv.news_published_date AS DATE) AS published_date, nkv.ent_text, nkv.ent_type 
+	ner_df = query_from_db("""SELECT nkv.published_date, nkv.ent_text, nkv.ent_type 
 		FROM news_db.news_kw_view nkv
-		WHERE CAST(nkv.news_published_date AS DATE) = '{}';""".format(selected_date))
+		WHERE nkv.published_date  = '{}';""".format(selected_date))
 	return ner_df
 
+def get_top_news(selected_date):
+	top_news_df = query_from_db("""SELECT aftv.news_id, aftv.news_title, nts.news_cluster, nrf.news_url FROM news_db.all_financial_title_view aftv
+	JOIN news_db.news_title_selection nts on nts.news_id = aftv.news_id
+	JOIN news_db.news_contents nc on nc.news_id = aftv.news_id
+	JOIN news_db.news_rss_feeds nrf on nrf.news_rss_feeds_id = nc.news_rss_feeds_id
+	WHERE aftv.published_date = '{}'""".format(str(selected_date)))
+	return top_news_df
 if __name__ == '__main__':
-	module = st.sidebar.selectbox('Mode: ', ['Crawling DB analysis', 'News Analysis (Basic Count)', 'New Analysis (TF-IDF)', 'New Analysis (NER Count)', 'News Analysis (companies)'])
+	module = st.sidebar.selectbox('Mode: ', ['Crawling DB analysis', 'News Analysis (Basic Count)', 'New Analysis (TF-IDF)', 'New Analysis (NER Count)', 'News Analysis (companies)', 'Top News'])
 	if module == 'Crawling DB analysis':
 		mode = st.sidebar.selectbox('Mode: ', ['Overview (Parse All)', 'Overview (Raw RSS)', 
 			'Detailed (Raw RSS)', 'Overview (Parse Success)', 'Datailed (Parse Success)', 'Overview (Parse Failed)', 'Detailed (Parse Failed)'])
@@ -98,14 +101,14 @@ if __name__ == '__main__':
 			st.title("How many rss feed per day?")
 			last_date = max(count_df['DATE'])
 			first_date = min(count_df['DATE'])
-			start_date = st.date_input('Start Date: ', last_date - timedelta(days=3), min_value = first_date  , max_value = last_date)
+			start_date = st.date_input('Start Date: ', last_date - timedelta(days=0), min_value = first_date  , max_value = last_date)
 			end_date = st.date_input('End Date: ', last_date, min_value = first_date, max_value = last_date)
 			new_df = count_df[(count_df['DATE'] >= start_date) & (count_df['DATE'] <= end_date)]
-			top_5_source = new_df.groupby(['category_name']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
+			top_5_source = new_df.groupby(['rss_source']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
 			#st.write()
-			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['category_name'].unique().tolist(),top_5_source)
-			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['category_name'] == cat]['DATE'], y = new_df[new_df['category_name'] == cat]['COUNT']) 
-				for cat in count_df['category_name'].unique() if cat in selected_source])
+			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['rss_source'].unique().tolist(),top_5_source)
+			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['rss_source'] == cat]['DATE'], y = new_df[new_df['rss_source'] == cat]['COUNT']) 
+				for cat in count_df['rss_source'].unique() if cat in selected_source])
 			# Change the bar mode
 			fig.update_layout(barmode='group')
 			st.write(fig)
@@ -132,13 +135,13 @@ if __name__ == '__main__':
 			st.title("How many rss feed parsed successfully per day?")
 			last_date = max(success_df['DATE'])
 			first_date = min(success_df['DATE'])
-			start_date = st.date_input('Start Date: ', last_date - timedelta(days=3), min_value = first_date  , max_value = last_date)
+			start_date = st.date_input('Start Date: ', last_date - timedelta(days=0), min_value = first_date  , max_value = last_date)
 			end_date = st.date_input('End Date: ', last_date, min_value = first_date, max_value = last_date)
 			new_df = success_df[(success_df['DATE'] >= start_date) & (success_df['DATE'] <= end_date)]
-			top_5_source = new_df.groupby(['category_name']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
-			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['category_name'].unique().tolist(),top_5_source)
-			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['category_name'] == cat]['DATE'], y = new_df[new_df['category_name'] == cat]['COUNT']) 
-				for cat in success_df['category_name'].unique() if cat in selected_source])
+			top_5_source = new_df.groupby(['rss_source']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
+			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['rss_source'].unique().tolist(),top_5_source)
+			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['rss_source'] == cat]['DATE'], y = new_df[new_df['rss_source'] == cat]['COUNT']) 
+				for cat in success_df['rss_source'].unique() if cat in selected_source])
 			# Change the bar mode
 			fig.update_layout(barmode='group')
 			st.write(fig)
@@ -166,13 +169,13 @@ if __name__ == '__main__':
 			st.title("How many rss feed parsed wrongly per day?")
 			last_date = max(fail_df['DATE'])
 			first_date = min(fail_df['DATE'])
-			start_date = st.date_input('Start Date: ', last_date - timedelta(days=3), min_value = first_date  , max_value = last_date)
+			start_date = st.date_input('Start Date: ', last_date - timedelta(days=0), min_value = first_date  , max_value = last_date)
 			end_date = st.date_input('End Date: ', last_date, min_value = first_date, max_value = last_date)
 			new_df = fail_df[(fail_df['DATE'] >= start_date) & (fail_df['DATE'] <= end_date)]
-			top_5_source = new_df.groupby(['category_name']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
-			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['category_name'].unique().tolist(),top_5_source)
-			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['category_name'] == cat]['DATE'], y = new_df[new_df['category_name'] == cat]['COUNT']) 
-				for cat in fail_df['category_name'].unique() if cat in selected_source])
+			top_5_source = new_df.groupby(['rss_source']).agg(sum).sort_values(by = ['COUNT']).index[-5:].values
+			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['rss_source'].unique().tolist(),top_5_source)
+			fig = go.Figure(data=[ go.Bar(name = cat, x = new_df[new_df['rss_source'] == cat]['DATE'], y = new_df[new_df['rss_source'] == cat]['COUNT']) 
+				for cat in fail_df['rss_source'].unique() if cat in selected_source])
 			# Change the bar mode
 			fig.update_layout(barmode='group')
 			st.write(fig)
@@ -180,19 +183,19 @@ if __name__ == '__main__':
 			st.title("What is the ratio that rss feed parsed wrongly per day?")
 			success_df_copy = success_df.rename(columns={"COUNT": "success_count"})
 			fail_df_copy = fail_df.rename(columns={"COUNT": "fail_count"})
-			all_df = pd.merge(success_df_copy, fail_df_copy, on=["DATE", "category_name"], how = 'outer')
+			all_df = pd.merge(success_df_copy, fail_df_copy, on=["DATE", "rss_source"], how = 'outer')
 			all_df.fillna(0, inplace = True)
 			all_df['all_count'] = all_df.apply(lambda x: x['success_count'] + x['fail_count'], axis = 1)
 			last_date = max(all_df['DATE'])
 			first_date = min(all_df['DATE'])
-			start_date = st.date_input('Start Date: ', last_date - timedelta(days=3), min_value = first_date  , max_value = last_date)
+			start_date = st.date_input('Start Date: ', last_date - timedelta(days=0), min_value = first_date  , max_value = last_date)
 			end_date = st.date_input('End Date: ', last_date, min_value = first_date, max_value = last_date)
 			new_df = all_df[(all_df['DATE'] >= start_date) & (all_df['DATE'] <= end_date)]
 
-			top_5_source = new_df.groupby(['category_name']).agg(sum).sort_values(by = ['all_count']).index[-5:].values
-			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['category_name'].unique().tolist(),top_5_source)
+			top_5_source = new_df.groupby(['rss_source']).agg(sum).sort_values(by = ['all_count']).index[-5:].values
+			selected_source = st.multiselect('The source you are interested in (Default is top 5 count source):', new_df['rss_source'].unique().tolist(),top_5_source)
 			#st.text([(([new_df[new_df['category_name'] == cat]['DATE'], new_df[new_df['category_name'] == cat]['category_name']]), new_df[new_df['category_name'] == cat][count])  for count in ('success_count', 'fail_count') for cat in count_df['category_name'].unique() if cat in selected_source])
-			fig = go.Figure(data=[ go.Bar(x = [new_df[new_df['category_name'] == cat]['DATE'], new_df[new_df['category_name'] == cat]['category_name']], y = new_df[new_df['category_name'] == cat][count], name = count)  for count in ('success_count', 'fail_count') for cat in count_df['category_name'].unique() if cat in selected_source])
+			fig = go.Figure(data=[ go.Bar(x = [new_df[new_df['rss_source'] == cat]['DATE'], new_df[new_df['rss_source'] == cat]['rss_source']], y = new_df[new_df['rss_source'] == cat][count], name = count)  for count in ('success_count', 'fail_count') for cat in count_df['rss_source'].unique() if cat in selected_source])
 			# Change the bar mode
 			fig.update_layout(barmode="stack")
 			st.write(fig)
@@ -200,7 +203,7 @@ if __name__ == '__main__':
 		kw_df = get_keywords_by_date()
 		st.title("Word of a Day")
 		last_date = max(kw_df['published_date'])
-		first_date = datetime.strptime("2021-01-19", "%Y-%m-%d")
+		first_date = datetime.strptime("2022-01-01", "%Y-%m-%d")
 		selected_date = st.date_input('Select date: ', last_date, min_value = first_date  , max_value = last_date)
 		stop_kw_list = set(['自由時報', '自由時報電子報', '大紀元', 'ltn', '自由娛樂', 'LTN', 'Liberty Times Net', 'nownews'])
 		
@@ -229,7 +232,7 @@ if __name__ == '__main__':
 		kw_df = get_keywords_by_date()
 		st.title("Word of a Day")
 		last_date = max(kw_df['published_date'])
-		first_date = datetime.strptime("2021-01-19", "%Y-%m-%d")
+		first_date = datetime.strptime("2022-01-01", "%Y-%m-%d")
 		selected_date = st.date_input('Select date: ', last_date, min_value = first_date  , max_value = last_date)
 		stop_kw_list = set(['自由時報', '自由時報電子報', '大紀元', 'ltn', '自由娛樂', 'LTN', 'Liberty Times Net', 'nownews'])
 		
@@ -254,7 +257,6 @@ if __name__ == '__main__':
 		fig.update_layout(font_size = 12)
 		st.write(fig)
 	elif module == 'New Analysis (NER Count)':
-		
 		end_date = datetime.now(tz = pytz.timezone('Asia/Taipei')).date()
 		start_date = datetime.strptime("2021-01-19", "%Y-%m-%d")
 		selected_date = st.date_input('Select date: ', end_date, min_value = start_date  , max_value = end_date)
@@ -283,9 +285,9 @@ if __name__ == '__main__':
 		start_date = datetime.strptime("2021-01-19", "%Y-%m-%d").date()
 		selected_date = st.date_input('Select date: ', end_date, min_value = start_date  , max_value = end_date)
 		
-		company_df = get_company_overview()
-		company_df = company_df[company_df['published_date'] == selected_date]
-		company_df = company_df.drop(columns = ['published_date'])
+		company_df = get_company_overview(selected_date)
+		#company_df = company_df[company_df['published_date'] == selected_date]
+		#company_df = company_df.drop(columns = ['published_date'])
 		company_df = company_df.set_index('company_name')
 		company_df = company_df['count'].astype(int)
 		company_df = company_df.sort_values(ascending = False)
@@ -299,6 +301,13 @@ if __name__ == '__main__':
 		fig = go.Figure([go.Bar(x = counts[::-1], y = words[::-1], orientation='h')])
 		fig.update_layout(font_size = 12)
 		st.write(fig)
+	elif module == 'Top News':
+		end_date = datetime.now(tz = pytz.timezone('Asia/Taipei')).date() - timedelta(days = 2)
+		start_date = datetime.strptime("2021-01-19", "%Y-%m-%d").date()
+		selected_date = st.date_input('Select date: ', end_date, min_value = start_date  , max_value = end_date)
+		df = get_top_news(selected_date)
 
+		topics = st.selectbox('Topics: ', df['news_cluster'].unique())
+		st.write(df.loc[df['news_cluster'] == topics])
 
 		
